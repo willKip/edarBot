a86f7f2f0fd5ce56438b370a2a31ae4ec423f1a9
willKip <coldbrushl@gmail.com>
2021-09-13 18:01:54 -0400

    Implement basic commands and logging
    Using the example below, the bot now has rudimentary commands and logs them. PyNaCl is logged to be missing; needs to be resolved.
    https://github.com/Rapptz/discord.py/blob/v1.7.3/examples/basic_bot.py

---
+++ edarbot.py
@@ -0,0 +1,69 @@
+# Multipurpose DnD Session Aid bot for personal use
+# William Lee
+
+# Requires the 'members' privileged intents
+
+import discord
+from discord.ext import commands
+import logging
+import random
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for _ in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+bot.run('ODg3MDc4MzQ0MjkyNjk2MDkw.YT-6Fg.2CWVdREOjMYRxV1VcrfX7lJZj9E')
