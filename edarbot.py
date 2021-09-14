898301e5d87b57c8b95e61f1632c701746e7b813
willKip <coldbrushl@gmail.com>
2021-09-13 21:32:15 -0400

    Implement basic YouTube audio, formatting
    Bot can join and have basic musicbot functionality, and single-quote
    strings now use double-quote for consistency.
    
    Improvement could be made in audio quality, consistency, and processing,
    as the bot takes a long time to process a youtube playlist given.
    
    Note: the previous play function, which played from local files,
    is replaced with the yt function, which inherits the name, considering how
    the bot will be used.

--- edarbot.py
+++ edarbot.py
@@ -8,46 +8,153 @@ import random
 import asyncio
 import youtube_dl
 
-# Suppress noise about console usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
-
 # Logging support
 # TODO: personalize logging
-logger = logging.getLogger('discord')
+logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 
 description = '''Personal multipurpose DnD Session Aid bot.\nIn memoriam of the fallen Robo Edar.'''
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+edarbot = commands.Bot(command_prefix="!", description=description, intents=intents)
+
+token = "ODg3MDc4MzQ0MjkyNjk2MDkw.YT-6Fg.2CWVdREOjMYRxV1VcrfX7lJZj9E"
+
+# Youtube_dl setup
+youtube_dl.utils.bug_reports_message = lambda: ''  # Suppress noise about console usage from errors
+
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0"  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    "options": "-vn"
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-@bot.event
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, url):
+        """Plays from a url (almost anything youtube_dl supports)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print("Player error: %s" % e) if e else None)
+
+        await ctx.send("Now playing: {}".format(player.title))
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send("Now playing: {}".format(player.title))
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send("Changed volume to {}%".format(volume))
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+
+@edarbot.event
 async def on_ready():
-    print(f"Logged in as \"{bot.user.name}\", ID {bot.user.id}")
+    print(f"Logged in as \"{edarbot.user}\", ID {edarbot.user.id}")
     print("Have a nice session!")
-    print('===---===')
+    print("===---===")
 
 
-@bot.command()
+@edarbot.command()
 async def roll(ctx, dice: str):
     """Rolls a dice in NdN format."""
     try:
-        rolls, limit = map(int, dice.split('d'))
+        rolls, limit = map(int, dice.split("d"))
     except (ValueError, Exception):
-        await ctx.send('Format has to be in NdN!')
+        await ctx.send("Format has to be in NdN!")
         return
 
-    result = ', '.join(str(random.randint(1, limit)) for _ in range(rolls))
+    result = ", ".join(str(random.randint(1, limit)) for _ in range(rolls))
     await ctx.send(result)
 
 
-@bot.command(description='For when you wanna settle the score some other way')
+@edarbot.command(description="For when you wanna settle the score some other way")
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
 
 
-bot.run('ODg3MDc4MzQ0MjkyNjk2MDkw.YT-6Fg.2CWVdREOjMYRxV1VcrfX7lJZj9E')
+edarbot.add_cog(Music(edarbot))
+edarbot.run(token)
